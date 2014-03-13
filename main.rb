1923c84d55cd40062d02595436db9b38aacbb6e3
Tim Herd <therd@blurb.com>
2014-03-13 12:51:01 -0700

    basic auth!

--- main.rb
+++ main.rb
@@ -1,6 +1,43 @@
 require 'sinatra'
+require 'sinatra/cookies'
 require 'twilio-ruby'
 require './twilio_stub'
+require 'pry'
+
+# from http://www.sinatrarb.com/faq.html#auth
+helpers do
+
+  def passwords
+    ENV['PLAINTEXT_passwords'].split(",") #note: this app has shitty insecure password management. Proceed accordingly
+  rescue #env variable not defined
+    nil
+  end
+
+
+  def protected!
+    return if authorized?
+    headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
+    halt 401, "Not authorized\n"
+  end
+
+  def authorized?
+    if cookies[:password]  #check for saved auth token
+      return passwords.include? cookies[:password]
+    else #log in
+      @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+      if @auth.provided? and @auth.basic? and @auth.credentials #and @auth.credentials == ['admin', 'admin']
+        # ignore username, because of laziness
+        password = @auth.credentials[-1]
+        if passwords.include? password
+          cookies[:password] = password
+          return true
+        else
+          redirect to('/jail') and return false
+        end
+      end
+    end
+  end
+end
 
 def number(name) #looks up phone number for name in ENV
   ENV[name]
@@ -15,8 +52,7 @@ class PHS #utility namespace
   end
 end
 
-PHONE_TO_LOCATE = "+16502753739" # 1.650.275.EQDW
-ORIGIN          = "+16505294993" # twilio number
+ORIGIN      = "+16505294993" # twilio number
 
 ACCOUNT_SID = ENV['sid']
 AUTH_TOKEN  = ENV['token']
@@ -24,22 +60,30 @@ AUTH_TOKEN  = ENV['token']
 CLIENT = TwilioFactory.get_client(ACCOUNT_SID, AUTH_TOKEN)
 
 get '/' do
+  protected!
   erb :landing
 end
 
-get '/call_contents' do
+get '/jail' do
+  "LOG IN NEXT TIME"
+end
+
+def call_contents
   Twilio::TwiML::Response.new do |r|
     r.Say "I am trying to locate your phone. If you are hearing this, this means you found it"
   end.text
+end
+
+get '/call_contents' do
+  call_contents
 end
 
 post '/call_contents' do
-  Twilio::TwiML::Response.new do |r|
-    r.Say "I am trying to locate your phone. If you are hearing this, this means you found it"
-  end.text
+  call_contents
 end
 
 post '/call' do
+  protected!
   puts "DEBUG: calling #{params[:name]} at #{number(params[:name])}"
   if number(params[:name])
     CLIENT.account.calls.create(
